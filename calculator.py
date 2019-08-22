d2bae80128e18f7dbffe1b17b9069ed4f39153f5
keanubowker8 <keanu.bowker@umuzi.org>
2019-08-22 11:36:01 +0200

    second commit

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 def add(*args):
+	'''takes any amount of numbers passed and adds them'''
 	sum = 0
 	for number in args:
 		sum += number
@@ -6,6 +7,7 @@ def add(*args):
 
 
 def multiply(*args):
+	'''takes any amount of numbers passed and multiplies them'''
 	sum = 1
 	for number in args:
 		sum *= number
--- calculator_test.py
+++ calculator_test.py
@@ -1,11 +1,16 @@
 from calculator import add, multiply
 
 def test_add():
+	'''test to make sure function can add 2 numbers'''
 	assert add(0,0) == 0
-	assert add(-1,-1) == -2
-	assert add(4,5) == 9	
-	assert add(2,3,3)
+	assert add(4,5) == 9
+	'''test to make sure function can add negative numbers'''
+	assert add(-1,-1) == -2	
+	'''test to make sure function can add many numbers'''
+	assert add(2,3,3)== 8
 	
 def test_multiply():
-    assert multiply(1,2)
-    assert multiply(1,2,3,4)	
+	'''test to make sure function can multiply 2 numbers'''
+	assert multiply(1,2) == 2
+	'''test to make sure function can multiply many numbers'''
+	assert multiply(1,2,3,4) == 24	
